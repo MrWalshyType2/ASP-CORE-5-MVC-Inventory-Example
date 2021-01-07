fe4b8a56967950093d74ada9939c3a9a0c94759a
MrWalshyType2 <mwalsh@academytrainee.com>
2021-01-07 18:11:37 +0000

    Added NuGet package for SqlServer, closes #4. Created context class for Item model, closes #5.

---
+++ Data/MvcItemContext.cs
@@ -0,0 +1,19 @@
+﻿using InventoryAppMvc.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryAppMvc.Data
+{
+    public class MvcItemContext : DbContext
+    {
+        public MvcItemContext(DbContextOptions<MvcItemContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Item> Item { get; set; }
+    }
+}
