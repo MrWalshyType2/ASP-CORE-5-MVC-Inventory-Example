e4acc3fdd44357089523bee961d0553f8a4600e5
MrWalshyType2 <mwalsh@academytrainee.com>
2021-01-07 17:15:33 +0000

    Added Item model. Closes #3

---
+++ Models/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryAppMvc.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt { get; }
+
+        public decimal Price { get; set; }
+    }
+}
