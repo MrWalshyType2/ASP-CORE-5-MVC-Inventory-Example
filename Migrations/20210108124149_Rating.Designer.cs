367b19db3d781f1bfc1cbb2b18b4a42a7fe9f6d1
MrWalshyType2 <mwalsh@academytrainee.com>
2021-01-08 12:44:57 +0000

    Migrated schema, closes #14

--- Migrations/20210108124149_Rating.Designer.cs
+++ Migrations/20210108124149_Rating.Designer.cs
@@ -4,14 +4,16 @@ using InventoryAppMvc.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace InventoryAppMvc.Migrations
 {
     [DbContext(typeof(MvcItemContext))]
-    partial class MvcItemContextModelSnapshot : ModelSnapshot
+    [Migration("20210108124149_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210108124149_Rating.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InventoryAppMvc.Migrations
+{
+    public partial class Rating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "Item",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Item");
+        }
+    }
+}
