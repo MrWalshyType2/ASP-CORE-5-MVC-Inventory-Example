4ef3e8dcb3de1cc0600c0e0c4bed3a618a9594c7
MrWalshyType2 <mwalsh@academytrainee.com>
2021-01-08 13:05:31 +0000

    Added validation logic to Item model, closes #15

--- Models/Item.cs
+++ Models/Item.cs
@@ -11,13 +11,18 @@ namespace InventoryAppMvc.Models
     public class Item
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
         public string Name { get; set; }
+
+        [StringLength(256, MinimumLength = 8)]
         public string Description { get; set; }
 
         private DateTime createdAt;
 
-        //[ReadOnly(true)]
-        [DataType(DataType.Date)]
+        [Editable(false)]
+        [Display(Name = "Created At"), DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd-mm-yyyy}")]
         public DateTime CreatedAt
         {
@@ -34,9 +39,16 @@ namespace InventoryAppMvc.Models
             }
         }
 
+        [Required]
         public Category Category { get; set; }
 
+        [Required]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        [Required]
+        [Range(0, 5)]
         public int Rating { get; set; }
     }
 }
